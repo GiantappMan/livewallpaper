d5241700638ae8c7ca2bdd0a347b937f45bd2d55
snowlab <snowlab@hotmail.com>
2018-08-08 18:02:49 +0800

    配置文件

---
+++ LiveWallpaper/Configs/GeneralSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveWallpaper.Configs
+{
+    class GeneralSetting
+    {
+        public bool StartupWithWindows { get; set; }
+    }
+}
