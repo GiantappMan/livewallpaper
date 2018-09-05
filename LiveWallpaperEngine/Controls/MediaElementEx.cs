4d11b3e090007d3f4b25caa35ed5938b42c185fc
DaZiYuan <mscoder@hotmail.com>
2018-09-05 14:56:04 +0800

    视频播放窗口

---
+++ LiveWallpaperEngine/Controls/MediaElementEx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LiveWallpaperEngine.Controls
+{
+    public class MediaElementEx : MediaElement
+    {
+        public MediaElementEx()
+        {
+            UnloadedBehavior = MediaState.Play;
+            MediaEnded += MediaElementEx_MediaEnded;
+        }
+
+        private void MediaElementEx_MediaEnded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var mediaElement = sender as MediaElement;
+            mediaElement.Position = new TimeSpan(0, 0, 0);
+        }
+    }
+}
---
+++ LiveWallpaperEngine/Controls/RenderWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LiveWallpaperEngine.Controls
+{
+    /// <summary>
+    /// Interaction logic for RenderWindow.xaml
+    /// </summary>
+    public partial class RenderWindow : Window
+    {
+        public RenderWindow()
+        {
+            InitializeComponent();
+            Loaded += RenderWindow_Loaded;
+        }
+
+        private void RenderWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= RenderWindow_Loaded;
+
+            double width = Screen.AllScreens[0].Bounds.Width;
+            double height = Screen.AllScreens[0].Bounds.Height;
+
+            Top = -4;
+            Left = 0;
+            Width = width;
+            Height = height;
+
+            WindowState = WindowState.Maximized;
+        }
+
+        #region Wallpaper
+
+        public Wallpaper Wallpaper
+        {
+            get { return (Wallpaper)GetValue(WallpaperProperty); }
+            set { SetValue(WallpaperProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Wallpaper.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WallpaperProperty =
+            DependencyProperty.Register("Wallpaper", typeof(Wallpaper), typeof(RenderWindow), new PropertyMetadata(null));
+
+        #endregion
+
+        public new void Show()
+        {
+            base.Show();
+
+        }
+    }
+}
