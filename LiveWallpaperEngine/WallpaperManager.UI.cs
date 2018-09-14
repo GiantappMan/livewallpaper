c54cf7721d9a496cc2a4f23facbae239328b4df6
snowlab <snowlab@hotmail.com>
2018-09-14 15:24:46 +0800

    优化

--- LiveWallpaperEngine/WallpaperManager.UI.cs
+++ LiveWallpaperEngine/WallpaperManager.UI.cs
@@ -5,6 +5,7 @@ using LiveWallpaperEngine.Controls;
 using LiveWallpaperEngine.NativeWallpapers;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -22,9 +23,13 @@ namespace LiveWallpaperEngine
         private static Wallpaper _lastwallPaper;
         private static SetWinEventHookDelegate _hookCallback;
         private static IntPtr _hook;
+        private static Process _currentProcess;
 
         public static void Show(Wallpaper wallpaper)
         {
+            if (_currentProcess == null)
+                _currentProcess = Process.GetCurrentProcess();
+
             IntPtr handler = IntPtr.Zero;
             Execute.OnUIThread(() =>
             {
@@ -39,6 +44,7 @@ namespace LiveWallpaperEngine
                 else
                 {
                     RenderWindow.Wallpaper = wallpaper;
+                    RenderWindow.Visibility = System.Windows.Visibility.Visible;
                 }
 
                 handler = new WindowInteropHelper(RenderWindow).Handle;
@@ -47,7 +53,7 @@ namespace LiveWallpaperEngine
                 {
                     //监控其他程序是否最大化
                     _hookCallback = new SetWinEventHookDelegate(WinEventProc);
-                    _hook = USER32Wrapper.SetWinEventHook(SetWinEventHookEventType.EVENT_OBJECT_LOCATIONCHANGE,
+                    _hook = User32Wrapper.SetWinEventHook(SetWinEventHookEventType.EVENT_SYSTEM_FOREGROUND,
                         SetWinEventHookEventType.EVENT_OBJECT_LOCATIONCHANGE, IntPtr.Zero, _hookCallback, 0, 0, SetWinEventHookFlag.WINEVENT_OUTOFCONTEXT);
                 }
             });
@@ -55,6 +61,7 @@ namespace LiveWallpaperEngine
             HandlerWallpaper.Show(handler);
         }
 
+        private static List<int> maximizedPid = new List<int>();
         private static void WinEventProc(IntPtr hook, SetWinEventHookEventType eventType, IntPtr window, int objectId, int childId, uint threadId, uint time)
         {
             try
@@ -65,18 +72,49 @@ namespace LiveWallpaperEngine
                 if (eventType == SetWinEventHookEventType.EVENT_OBJECT_LOCATIONCHANGE)
                 {
                     WINDOWPLACEMENT placment = new WINDOWPLACEMENT();
-                    USER32Wrapper.GetWindowPlacement(window, ref placment);
-                    if (placment.showCmd == (uint)WINDOWPLACEMENTFlags.SW_SHOWMAXIMIZED)
+                    User32Wrapper.GetWindowPlacement(window, ref placment);
+                    string title = User32Wrapper.GetWindowText(window);
+                    int pid = User32Wrapper.GetProcessId(window);
+                    if (placment.showCmd == WINDOWPLACEMENTFlags.SW_HIDE)
+                        return;
+
+                    if (pid == _currentProcess.Id)
+                        return;
+
+                    //if (title == nameof(RenderWindow))
+                    //    return;
+                    //System.Diagnostics.Debug.WriteLine(title);
+
+                    if (placment.showCmd == WINDOWPLACEMENTFlags.SW_SHOWMAXIMIZED)
                     {
-                        var handle = USER32Wrapper.GetForegroundWindow();
-                        string txt = USER32Wrapper.GetWindowText(handle);
-                        System.Diagnostics.Debug.WriteLine(txt);
+                        if (!maximizedPid.Contains(pid))
+                            maximizedPid.Add(pid);
+                        //var handle = USER32Wrapper.GetForegroundWindow();
+                        //string txt = USER32Wrapper.GetWindowText(handle);
+                        //System.Diagnostics.Debug.WriteLine("foreground:" + txt);
                     }
 
-                    if (placment.showCmd == (uint)WINDOWPLACEMENTFlags.SW_RESTORE ||
-                        placment.showCmd == (uint)WINDOWPLACEMENTFlags.SW_SHOW ||
-                        placment.showCmd == (uint)WINDOWPLACEMENTFlags.SW_SHOWMINIMIZED)
-                        System.Diagnostics.Debug.WriteLine("离开" + placment.showCmd);
+                    if (placment.showCmd == WINDOWPLACEMENTFlags.SW_SHOWNORMAL ||
+                        placment.showCmd == WINDOWPLACEMENTFlags.SW_RESTORE ||
+                        placment.showCmd == WINDOWPLACEMENTFlags.SW_SHOW ||
+                        placment.showCmd == WINDOWPLACEMENTFlags.SW_SHOWMINIMIZED)
+                    {
+                        //System.Diagnostics.Debug.WriteLine("离开" + placment.showCmd);
+                        if (maximizedPid.Contains(pid))
+                            maximizedPid.Remove(pid);
+                    }
+                    //else
+                    System.Diagnostics.Debug.WriteLine($"Other:{title}.pid:{pid}. cmd:{placment.showCmd }. {DateTime.Now}");
+                    if (maximizedPid.Count == 0)
+                        System.Diagnostics.Debug.WriteLine("无");
+                    else
+                    {
+                        foreach (var item in maximizedPid)
+                        {
+                            System.Diagnostics.Debug.Write($"{item} ");
+                        }
+                        System.Diagnostics.Debug.WriteLine("");
+                    }
                 }
             }
             catch (Exception ex)
@@ -101,12 +139,13 @@ namespace LiveWallpaperEngine
 
             Execute.OnUIThread(() =>
             {
+                RenderWindow.Visibility = System.Windows.Visibility.Collapsed;
                 RenderWindow.Wallpaper = null;
             });
 
             if (_hook != IntPtr.Zero)
             {
-                bool ok = USER32Wrapper.UnhookWinEvent(_hook);
+                bool ok = User32Wrapper.UnhookWinEvent(_hook);
             }
             HandlerWallpaper.Close();
         }
