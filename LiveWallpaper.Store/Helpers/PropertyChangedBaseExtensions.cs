54aea6c96fe68d5293bcc26d3a526f967be8d725
DaZiYuan <mscoder@hotmail.com>
2018-10-04 23:28:15 +0800

    增加store项目

---
+++ LiveWallpaper.Store/Helpers/PropertyChangedBaseExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+using Caliburn.Micro;
+
+namespace LiveWallpaper.Store.Helpers
+{
+    public static class PropertyChangedBaseExtensions
+    {
+        public static bool Set<T>(this PropertyChangedBase propertyChangedBase, ref T oldValue, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(oldValue, newValue))
+            {
+                return false;
+            }
+
+            oldValue = newValue;
+            propertyChangedBase.NotifyOfPropertyChange(propertyName);
+
+            return true;
+        }
+    }
+}
