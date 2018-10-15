265b7e3da4c53a7de3aad1b88ed27c4430f40a36
DaZiYuan <mscoder@hotmail.com>
2018-10-15 10:58:37 +0800

    读写文件

--- LiveWallpaper.Store/ViewModels/MainViewModel.cs
+++ LiveWallpaper.Store/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -6,6 +7,7 @@ using Caliburn.Micro;
 using LiveWallpaper.Server;
 using LiveWallpaper.Store.Helpers;
 using LiveWallpaper.Store.Services;
+using Windows.Storage;
 
 namespace LiveWallpaper.Store.ViewModels
 {
@@ -58,9 +60,38 @@ namespace LiveWallpaper.Store.ViewModels
             Server.InitServer();
         }
 
-        public void Install()
+        public async void Install()
         {
+            try
+            {
+                //写入文件
+                string dir = "d:\\";
+                string fileName = "sample.txt";
+                string fullPath = Path.Combine(dir, fileName);
+                StorageFolder storageFolder = await StorageFolder.GetFolderFromPathAsync(dir);
+                StorageFile writeFile = await storageFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+                await FileIO.WriteTextAsync(writeFile, DateTime.Now.ToString());
 
+                //读取文件
+                StorageFile readFile = await storageFolder.GetFileAsync(fileName);
+
+                //全路径读取
+                //StorageFile readFile = await StorageFile.GetFileFromPathAsync(fullPath);
+                var stream = await readFile.OpenAsync(FileAccessMode.Read);
+                ulong size = stream.Size;
+                using (var inputStream = stream.GetInputStreamAt(0))
+                {
+                    using (var dataReader = new Windows.Storage.Streams.DataReader(inputStream))
+                    {
+                        uint numBytesLoaded = await dataReader.LoadAsync((uint)size);
+                        string text = dataReader.ReadString(numBytesLoaded);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
 
         public void Setting()
