cc6e8490b5ad9def2ccbec3c4c66cd3054df124d
DaZiYuan <mscoder@hotmail.com>
2020-12-14 23:37:38 +0800

    上传文件

---
+++ LiveWallpaperCore.LocalServer/Controllers/LiveWallpaperController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveWallpaperCore.LocalServer.Controllers
+{
+    public class LiveWallpaperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UploadFiles(IFormCollection files)
+        {
+            return Json(files.Files.Count);
+        }
+    }
+}
