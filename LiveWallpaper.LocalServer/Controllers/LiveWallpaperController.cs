f1118b0e6dc57b5521c63b9ec29e1a76ac612522
DaZiYuan <mscoder@hotmail.com>
2020-12-29 22:18:02 +0800

    通用上传文件接口

--- LiveWallpaper.LocalServer/Controllers/LiveWallpaperController.cs
+++ LiveWallpaper.LocalServer/Controllers/LiveWallpaperController.cs
@@ -17,47 +17,84 @@ namespace LiveWallpaper.LocalServer.Controllers
 
         [RequestSizeLimit(10L * 1024L * 1024L * 1024L)]
         [RequestFormLimits(MultipartBodyLengthLimit = 10L * 1024L * 1024L * 1024L)]
-        public async Task<WallpaperModel> CreateWallpaperDraft(IFormCollection fc)
+        public async Task<BaseApiResult<string>> UploadFile(IFormCollection fc)
         {
-            if (fc != null && fc.Files.Count > 0 && fc.Files[0].Length > 0)
-            {
-                var formFile = fc.Files[0];
-                var info = new WallpaperProjectInfo()
-                {
-                    File = formFile.FileName
-                };
-                var targetDir = await WallpaperApi.CreateWallpaperDraft(AppManager.UserSetting.Wallpaper.WallpaperSaveDir, info);
-                var distFile = Path.Combine(targetDir, formFile.FileName);
-                using var stream = System.IO.File.Create(distFile);
-                await formFile.CopyToAsync(stream);
+            string distDir = fc["distDir"];
+            if (string.IsNullOrEmpty(distDir))
+                return BaseApiResult<string>.ErrorState(ErrorType.Failed, "distPath can not be empty");
 
-                var r = WallpaperApi.CreateWallpaperModel(distFile);
-                if (string.IsNullOrEmpty(r.Info.Title))
+            //string dir = Path.GetDirectoryName(distDir);
+            //不能操作非壁纸目录
+            if (!distDir.Contains(AppManager.UserSetting.Wallpaper.WallpaperSaveDir))
+                return BaseApiResult<string>.ErrorState(ErrorType.Failed);
+
+            try
+            {
+                if (fc != null && fc.Files.Count > 0 && fc.Files[0].Length > 0)
                 {
-                    int lastIndex = r.Info.File.LastIndexOf(".");
-                    r.Info.Title = r.Info.File.Remove(lastIndex);
+                    var formFile = fc.Files[0];
+                    var info = new WallpaperProjectInfo()
+                    {
+                        File = formFile.FileName
+                    };
+                    string distPath = Path.Combine(distDir, formFile.FileName);
+                    using var stream = System.IO.File.Create(distPath);
+                    await formFile.CopyToAsync(stream);
+
+                    return BaseApiResult<string>.SuccessState(distPath);
                 }
 
-                return r;
+                return BaseApiResult<string>.ErrorState(ErrorType.Failed);
             }
-            return null;
-        }
-
-        [RequestSizeLimit(5 * 1024L * 1024L)]
-        [RequestFormLimits(MultipartBodyLengthLimit = 5 * 1024L * 1024L)]
-        public async Task<string> UploadTmpFile(IFormCollection fc)
-        {
-            if (fc != null && fc.Files.Count > 0 && fc.Files[0].Length > 0)
+            catch (System.Exception ex)
             {
-                var formFile = fc.Files[0];
-                string fileName = formFile.FileName;
-                var distFile = Path.Combine(Path.GetTempPath(), fileName);
-                using var stream = System.IO.File.Create(distFile);
-                await formFile.CopyToAsync(stream);
-
-                return distFile;
+                return BaseApiResult<string>.ExceptionState(ex);
             }
-            return null;
         }
+
+        //[RequestSizeLimit(10L * 1024L * 1024L * 1024L)]
+        //[RequestFormLimits(MultipartBodyLengthLimit = 10L * 1024L * 1024L * 1024L)]
+        //public async Task<WallpaperModel> CreateWallpaperDraft(IFormCollection fc)
+        //{
+        //    if (fc != null && fc.Files.Count > 0 && fc.Files[0].Length > 0)
+        //    {
+        //        var formFile = fc.Files[0];
+        //        var info = new WallpaperProjectInfo()
+        //        {
+        //            File = formFile.FileName
+        //        };
+        //        var targetDir = await WallpaperApi.CreateWallpaperDraft(AppManager.UserSetting.Wallpaper.WallpaperSaveDir, info);
+        //        var distFile = Path.Combine(targetDir, formFile.FileName);
+        //        using var stream = System.IO.File.Create(distFile);
+        //        await formFile.CopyToAsync(stream);
+
+        //        var r = WallpaperApi.CreateWallpaperModel(distFile);
+        //        if (string.IsNullOrEmpty(r.Info.Title))
+        //        {
+        //            int lastIndex = r.Info.File.LastIndexOf(".");
+        //            r.Info.Title = r.Info.File.Remove(lastIndex);
+        //        }
+
+        //        return r;
+        //    }
+        //    return null;
+        //}
+
+        //[RequestSizeLimit(5 * 1024L * 1024L)]
+        //[RequestFormLimits(MultipartBodyLengthLimit = 5 * 1024L * 1024L)]
+        //public async Task<string> UploadTmpFile(IFormCollection fc)
+        //{
+        //    if (fc != null && fc.Files.Count > 0 && fc.Files[0].Length > 0)
+        //    {
+        //        var formFile = fc.Files[0];
+        //        string fileName = formFile.FileName;
+        //        var distFile = Path.Combine(Path.GetTempPath(), fileName);
+        //        using var stream = System.IO.File.Create(distFile);
+        //        await formFile.CopyToAsync(stream);
+
+        //        return distFile;
+        //    }
+        //    return null;
+        //}
     }
 }
