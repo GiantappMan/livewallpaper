29cae0450442f653f4725d91700b36164ad48497
DaZiYuan <mscoder@hotmail.com>
2021-02-28 22:05:57 +0800

    支持多语言切换

--- LiveWallpaper/AppContext.cs
+++ LiveWallpaper/AppContext.cs
@@ -4,8 +4,7 @@ using NLog;
 using System;
 using System.Diagnostics;
 using System.Drawing;
-using System.Net;
-using System.Net.Sockets;
+using System.IO;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,19 +20,22 @@ namespace LiveWallpaper
         private ContextMenuStrip _contextMenu;
         private ToolStripMenuItem _btnMainUI;
         private ToolStripMenuItem _btnCommunity;
+        private ToolStripMenuItem _btnSetting;
         private ToolStripMenuItem _btnExit;
         private System.ComponentModel.IContainer _components;
         #endregion
 
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         private static readonly LanService _lanService = new LanService();
+        private static Dispatcher _uiDispatcher;
         private static Mutex _mutex;
 
         public AppContext()
         {
+            _uiDispatcher = Dispatcher.CurrentDispatcher;
             InitializeUI();
 
-            WallpaperApi.Initlize(Dispatcher.CurrentDispatcher);
+            WallpaperApi.Initlize(_uiDispatcher);
             AppManager.CultureChanged += LanService_CultureChanged;
             SetMenuText();
             _ = Task.Run(() =>
@@ -44,7 +46,7 @@ namespace LiveWallpaper
             CheckMutex();
         }
 
-        private void CheckMutex()
+        private async void CheckMutex()
         {
             try
             {
@@ -52,7 +54,7 @@ namespace LiveWallpaper
 
                 if (!ret)
                 {
-                    _notifyIcon.ShowBalloonTip(5, "提示", "已有一个实例启动，请查看右下角托盘", ToolTipIcon.Warning);
+                    _notifyIcon.ShowBalloonTip(5, await GetText("common.information"), await GetText("client.started"), ToolTipIcon.Info);
                     Environment.Exit(0);
                     return;
                 }
@@ -72,16 +74,24 @@ namespace LiveWallpaper
             _components = new System.ComponentModel.Container();
             _contextMenu = new ContextMenuStrip();
 
-            _btnCommunity = new ToolStripMenuItem()
-            ;
+            //_contextMenu.Items.Add(new ToolStripSeparator());
+
+            _btnCommunity = new ToolStripMenuItem();
             _btnCommunity.Click += BtnCommunity_Click;
             _contextMenu.Items.Add(_btnCommunity);
 
-            _btnMainUI = new ToolStripMenuItem()
-            ;
+            _btnMainUI = new ToolStripMenuItem();
             _btnMainUI.Click += BtnMainUI_Click;
             _contextMenu.Items.Add(_btnMainUI);
 
+            _btnSetting = new ToolStripMenuItem();
+            _btnSetting.Click += BtnSetting_Click;
+            string dir = Path.GetDirectoryName(Application.ExecutablePath);
+            string imgPath = Path.Combine(dir, "Assets", "setting.png");
+            _btnSetting.Image = Image.FromFile(imgPath);
+            _contextMenu.Items.Add(_btnSetting);
+
+            _contextMenu.Items.Add(new ToolStripSeparator());
 
             _btnExit = new ToolStripMenuItem();
             _btnExit.Click += BtnExit_Click;
@@ -98,20 +108,27 @@ namespace LiveWallpaper
             _notifyIcon.MouseClick += new MouseEventHandler(NotifyIcon_MouseClick);
         }
 
-        private async void SetMenuText()
+        private void SetMenuText()
+        {
+            _ = _uiDispatcher.Invoke(async () =>
+              {
+                  _btnCommunity.Text = await GetText("community.title");
+                  _btnMainUI.Text = await GetText("local.title");
+                  _btnExit.Text = await GetText("client.exit");
+                  _btnSetting.Text = await GetText("common.settings");
+                  _notifyIcon.Text = await GetText("common.appName");
+              });
+        }
+
+        private async Task<string> GetText(string key)
         {
             if (AppManager.UserSetting == null)
             {
                 await AppManager.LoadUserSetting();
             }
             string culture = AppManager.UserSetting.General.CurrentLan ?? Thread.CurrentThread.CurrentCulture.Name;
-            _ = Dispatcher.CurrentDispatcher.Invoke(async () =>
-              {
-                  _btnCommunity.Text = await _lanService.GetText("community.title", culture);
-                  _btnMainUI.Text = await _lanService.GetText("local.title", culture);
-                  _btnExit.Text = await _lanService.GetText("client.exit", culture);
-                  _notifyIcon.Text = await _lanService.GetText("common.appName", culture);
-              });
+            var r = await _lanService.GetText(key, culture);
+            return r;
         }
 
         private void LanService_CultureChanged(object sender, EventArgs e)
@@ -137,20 +154,23 @@ namespace LiveWallpaper
 
         private void BtnCommunity_Click(object sender, EventArgs e)
         {
-            try
-            {
-                Process.Start(new ProcessStartInfo("https://livewallpaper.giantapp.cn/wallpapers") { UseShellExecute = true });
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
+            OpenUrl("https://livewallpaper.giantapp.cn/wallpapers");
         }
         private void BtnMainUI_Click(object sender, EventArgs e)
         {
+            OpenUrl("https://livewallpaper.giantapp.cn/local");
+        }
+
+        private void BtnSetting_Click(object sender, EventArgs e)
+        {
+            OpenUrl("https://livewallpaper.giantapp.cn/dashboard/client/setting");
+        }
+
+        private void OpenUrl(string url)
+        {
             try
             {
-                Process.Start(new ProcessStartInfo("https://livewallpaper.giantapp.cn/local") { UseShellExecute = true });
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
             }
             catch (Exception ex)
             {
