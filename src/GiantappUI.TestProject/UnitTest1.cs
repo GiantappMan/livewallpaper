a7ecb0320f68faf4176aa423e75b0bd93acc23b1
DaZiYuan <mscoder@hotmail.com>
2022-11-14 00:50:23 +0800

    创建 测试工程

---
+++ src/GiantappUI.TestProject/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GiantappUI.TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/LiveWallpaper.TestProject/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LiveWallpaper.TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
