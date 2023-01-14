1f379356a1b04cb4644553d291e8e6fb9a5e9680
DaZiYuan <mscoder@hotmail.com>
2023-01-14 23:52:05 +0800

    chore: systemtray研究

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,16 +2,71 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-
+use tauri::{AppHandle, CustomMenuItem, Manager, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{SystemTray, SystemTrayEvent};
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+fn create_system_tray() -> SystemTray {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(hide)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    SystemTray::new().with_menu(tray_menu)
+}
+
+fn system_tray_handler(app: &AppHandle, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::MenuItemClick { id, .. } => {
+            if id == "quit" {
+                std::process::exit(0);
+            } else if id == "hide" {
+                let window = app.get_window("main").unwrap();
+                window.hide().unwrap();
+            }
+        }
+        SystemTrayEvent::DoubleClick {
+            position: _,
+            size: _,
+            ..
+        } => {
+            let window = app.get_window("main");
+            if window.is_some() {
+                window.unwrap().show().unwrap();
+            } else {
+                let main_window = tauri::WindowBuilder::new(
+                    app,
+                    "main",
+                    tauri::WindowUrl::App("index.html".into()),
+                )
+                .build()
+                .expect("failed to create main window");
+                main_window.set_title("LiveWallpaper3").unwrap();
+                main_window.center().unwrap();
+                main_window.show().unwrap();
+            }
+        }
+        _ => {}
+    }
+}
+
 fn main() {
     tauri::Builder::default()
+        .system_tray(create_system_tray())
+        .on_system_tray_event(system_tray_handler)
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|_app_handle, event| match event {
+            //Keep the app running in the background after closing all windows
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
