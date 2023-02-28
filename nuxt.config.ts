9c5d6d4654aaaf434197743db4e25caa2f9a14aa
DaZiYuan <mscoder@hotmail.com>
2023-03-01 00:06:53 +0800

    chore:引用native-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,19 @@
 export default defineNuxtConfig({
     srcDir: 'src/',
     modules: [
-        '@nuxtjs/tailwindcss'
+        '@nuxtjs/tailwindcss',
+        '@huntersofbook/naive-ui-nuxt'
     ],
     build: { transpile: ["@headlessui/vue"] },
+    // Optionally, specify global naive-ui config
+    // Supports options that are normally set via 'n-config-provider'
+    // https://www.naiveui.com/en-US/os-theme/docs/customize-theme
+    naiveUI: {
+        themeOverrides: {
+            common: {
+                primaryColor: '#ff0000',
+                primaryColorHover: '#8b0000'
+            }
+        }
+    }
 })
--- src/pages/community.vue
+++ src/pages/community.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
+        <NConfigProvider :theme="darkTheme">
+            <NGlobalStyle />
+            <div>
+                Nuxt module playground!
+                <NButton>Default</NButton>
+            </div>
+        </NConfigProvider>
         建设中
     </div>
 </template>
-  
+<script setup lang="ts">
+import { darkTheme } from 'naive-ui'
+</script>
