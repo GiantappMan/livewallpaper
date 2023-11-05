24e0f2dae9818a0523563a317d71199d21189254
DaZiYuan <mscoder@hotmail.com>
2023-11-05 21:57:08 +0800

    chore:增加build到客户端命令

--- src/giantapp-wallpaper-ui/next.config.js
+++ src/giantapp-wallpaper-ui/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-     distDir: '../giantapp-wallpaper-client/Client/Assets/UI',
 }
 
 module.exports = nextConfig
