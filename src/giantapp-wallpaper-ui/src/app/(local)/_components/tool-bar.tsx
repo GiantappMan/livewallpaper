5a6b99a588fe49087a175dd9e645fbe1b4e3c761
DaZiYuan <mscoder@hotmail.com>
2023-12-24 15:22:26 +0800

    chore:多屏幕支持返回

--- src/giantapp-wallpaper-ui/src/app/(local)/_components/tool-bar.tsx
+++ src/giantapp-wallpaper-ui/src/app/(local)/_components/tool-bar.tsx
@@ -19,6 +19,9 @@ import {
 } from "@/components/ui/popover"
 import { Slider } from "@/components/ui/slider"
 import api from "@/lib/client/api";
+import {
+    Reply,
+} from "lucide-react"
 
 interface ToolBarProps extends React.HTMLAttributes<HTMLElement> {
     playingPlaylist: Playlist[]
@@ -36,6 +39,11 @@ export function ToolBar({ playingPlaylist, screens }: ToolBarProps) {
     const [playlists, setPlaylists] = useState<PlaylistWrapper[]>([]);
     const [isPaused, setIsPaused] = useState<boolean>(false);
     const [volume, setVolume] = useState<number>(0);
+    const [singleMode, setSingleMode] = useState<boolean>(true);
+
+    useEffect(() => {
+        setSingleMode(playlists.length === 1);
+    }, [playlists]);
 
     useEffect(() => {
         let tmpPlaylists: PlaylistWrapper[] = [];
@@ -127,19 +135,27 @@ export function ToolBar({ playingPlaylist, screens }: ToolBarProps) {
         <div className="flex flex-wrap flex-initial w-1/4 overflow-hidden h-full">
             {playlists.map((item, index) => {
                 const isSelected = selectedPlaylist?.current === item.current;
-                return <div key={index} className="flex items-center space-x-4 p-1">
-                    <picture onClick={() => isSelected ? setSelectedPlaylist(null) : setSelectedPlaylist(item)} className={cn({ "cursor-pointer": playlists.length > 1 })}>
-                        <img
-                            alt="Cover"
-                            title={item.current.meta?.title}
-                            className={cn(["rounded-lg object-scale-cover aspect-square", playlists.length > 1 && isSelected ? " border-2 border-primary" : ""])}
-                            height={50}
-                            src={item.current.coverUrl || "/wp-placeholder.webp"}
-                            width={50}
-                        />
-                    </picture>
+                return <div key={index} className="flex items-center p-1">
                     {
-                        (playlists.length === 1) && <div className="flex flex-col text-sm truncate">
+                        isSelected && <Button className="self-start -ml-4" variant="link" size="icon" onClick={() => setSelectedPlaylist(null)} title="返回列表">
+                            <Reply className="h-4 w-4" />
+                            <span className="sr-only">返回列表</span>
+                        </Button>
+                    }
+                    {
+                        (singleMode || (!selectedPlaylist || isSelected)) && <picture onClick={() => isSelected ? setSelectedPlaylist(null) : setSelectedPlaylist(item)} className={cn([{ "cursor-pointer": !singleMode }, "mr-2"])}>
+                            <img
+                                alt="Cover"
+                                title={item.current.meta?.title}
+                                className={cn(["rounded-lg object-scale-cover aspect-square", !singleMode && isSelected ? " border-2 border-primary" : ""])}
+                                height={50}
+                                src={item.current.coverUrl || "/wp-placeholder.webp"}
+                                width={50}
+                            />
+                        </picture>
+                    }
+                    {
+                        (singleMode || isSelected) && <div className="flex flex-col text-sm truncate">
                             <div className="font-semibold">{item?.current.meta?.title}</div>
                             <div >{item?.current.meta?.description}</div>
                         </div>
@@ -150,23 +166,25 @@ export function ToolBar({ playingPlaylist, screens }: ToolBarProps) {
 
         <div className="flex flex-col flex-1 w-1/2 items-center justify-between">
             <div className="space-x-4">
-                <Button variant="ghost" className="hover:text-primary" title="上一个壁纸">
-                    <svg
-                        className="h-5 w-5 "
-                        fill="none"
-                        height="24"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <polygon points="11 19 2 12 11 5 11 19" />
-                        <polygon points="22 19 13 12 22 5 22 19" />
-                    </svg>
-                </Button>
+                {
+                    <Button variant="ghost" className="hover:text-primary" title="上一个壁纸">
+                        <svg
+                            className="h-5 w-5 "
+                            fill="none"
+                            height="24"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <polygon points="11 19 2 12 11 5 11 19" />
+                            <polygon points="22 19 13 12 22 5 22 19" />
+                        </svg>
+                    </Button>
+                }
                 {
                     isPaused && <Button variant="ghost" className="hover:text-primary" title="播放" onClick={handlePlayClick}>
                         <svg
@@ -204,25 +222,27 @@ export function ToolBar({ playingPlaylist, screens }: ToolBarProps) {
                         </svg>
                     </Button>
                 }
-                <Button variant="ghost" className="hover:text-primary" title="下一个壁纸">
-                    <svg
-                        className=" h-5 w-5 "
-                        fill="none"
-                        height="24"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <polygon points="13 19 22 12 13 5 13 19" />
-                        <polygon points="2 19 11 12 2 5 2 19" />
-                    </svg>
-                </Button>
+                {
+                    <Button variant="ghost" className="hover:text-primary" title="下一个壁纸">
+                        <svg
+                            className=" h-5 w-5 "
+                            fill="none"
+                            height="24"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <polygon points="13 19 22 12 13 5 13 19" />
+                            <polygon points="2 19 11 12 2 5 2 19" />
+                        </svg>
+                    </Button>
+                }
             </div>
-            {(selectedPlaylist || playlists.length === 1) && <div className="flex items-center justify-between text-xs w-full">
+            {(selectedPlaylist || singleMode) && <div className="flex items-center justify-between text-xs w-full">
                 <div className="text-primary-600 dark:text-primary-400">00:00</div>
                 <div className="w-full h-1 mx-4 bg-primary/60 rounded" />
                 <div className="text-primary-600 dark:text-primary-400">04:30</div>
@@ -253,7 +273,7 @@ export function ToolBar({ playingPlaylist, screens }: ToolBarProps) {
                     <Slider value={[volume]} max={100} min={0} step={1} onValueChange={handleVolumeChange} />
                 </PopoverContent>
             </Popover>
-            {(selectedPlaylist || playlists.length === 1) && <>
+            {(selectedPlaylist || singleMode) && <>
                 <Sheet>
                     <SheetTrigger asChild>
                         <Button variant="ghost" className="hover:text-primary px-3" title="播放列表">
