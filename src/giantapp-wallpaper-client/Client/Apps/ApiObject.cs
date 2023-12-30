12bff090e8c0a52ccfda7b3fec4487d844885348
DaZiYuan <mscoder@hotmail.com>
2023-12-30 22:10:46 +0800

    chore:本地创建壁纸开发

--- src/giantapp-wallpaper-client/Client/Apps/ApiObject.cs
+++ src/giantapp-wallpaper-client/Client/Apps/ApiObject.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WallpaperCore;
--- src/giantapp-wallpaper-ui/src/app/(local)/_components/create-wallpaper-dialog.tsx
+++ src/giantapp-wallpaper-ui/src/app/(local)/_components/create-wallpaper-dialog.tsx
@@ -12,17 +12,30 @@ import {
 import { Input } from "@/components/ui/input"
 import { DeleteIcon, UploadCloudIcon } from "lucide-react"
 import { Progress } from "@/components/ui/progress"
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { cn } from "@/lib/utils"
 import api from "@/lib/client/api"
 import { toast } from "sonner"
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
+
+const formSchema = z.object({
+    title: z.string(),
+    file: z.instanceof(File, {
+        message: "文件未上传",
+    }).refine((file) => file.size < 1024 * 1024 * 1000, {
+        message: "文件大小不能超过10MB",
+    }),
+})
 
 interface CreateWallpaperDialogProps {
     open: boolean
     onChange: (open: boolean) => void
 }
 
-function processFile(file: File, onProgress: (progress: number) => void): Promise<string> {
+function processFile(file: File, onProgress: (progress: number) => void, controller: AbortController): Promise<string> {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.onload = async (e) => {
@@ -39,6 +52,11 @@ function processFile(file: File, onProgress: (progress: number) => void): Promis
                     let lastReportedProgress = 0; // 上次报告的进度
 
                     const processChunk = () => {
+                        if (controller.signal.aborted) {
+                            reject(new Error('Processing aborted'));
+                            return;
+                        }
+
                         const start = i;
                         const end = Math.min(i + chunkSize, contents.length);
                         const subArray = Array.from(contents.subarray(start, end));
@@ -71,40 +89,59 @@ function processFile(file: File, onProgress: (progress: number) => void): Promis
             }
         };
         reader.readAsArrayBuffer(file);
+
+        controller.signal.addEventListener('abort', () => {
+            reader.abort();
+            reject(new Error('导入已中止'));
+        });
     });
 }
 
+let abortController: AbortController | undefined = undefined;
+
 export function CreateWallpaperDialog(props: CreateWallpaperDialogProps) {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+            file: undefined
+        }
+    })
     const [isOver, setIsOver] = useState(false);
     const [progress, setProgress] = useState(0);
     const [uploading, setUploading] = useState(false);
+    const uploadingFile = form.watch("file");
     const [uploadedFile, setUploadedFile] = useState<string>();
-    const [title, setTitle] = useState<string>();
+    const fileInputRef = useRef<HTMLInputElement>(null);
 
     //每次打开重置状态
     useEffect(() => {
-        setIsOver(false);
+        if (!props.open) {
+            setIsOver(false);
+            setProgress(0);
+            setUploading(false);
+            setUploadedFile(undefined);
+            abortController?.abort();
+            form.reset();
+        }
+    }, [form, props.open]);
+
+    const uploadFile = useCallback(async (file: File) => {
         setProgress(0);
-        setUploading(false);
-        setUploadedFile(undefined);
-    }, [props.open]);
-
-    const handleDrop = useCallback(async (e: React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        setIsOver(false);
         setUploading(true);
+        setUploadedFile(undefined);
 
-        var file = e.dataTransfer?.files[0];
+        abortController?.abort();
+        abortController = new AbortController();
+
         const progressCallback = (progress: number) => {
-            console.log(`上传进度: ${progress}%`);
+            console.log(`导入进度: ${progress}%`);
             progress = Math.floor(progress);
             setProgress(progress);
         };
         try {
-            const base64String = await processFile(file, progressCallback);
-            console.log("上传", new Date());
+            const base64String = await processFile(file, progressCallback, abortController);
             await api.uploadToTmp(file.name, base64String);
-            console.log("已上传", new Date());
             setUploadedFile(file.name);
         } catch (e) {
             toast.error((e as any).message);
@@ -112,6 +149,11 @@ export function CreateWallpaperDialog(props: CreateWallpaperDialogProps) {
             setUploading(false);
         }
     }, []);
+
+    const handleDrop = useCallback(async (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        form.setValue("file", e.dataTransfer.files[0]);
+    }, [form]);
 
     const handleDragOver = useCallback((e: React.DragEvent<HTMLDivElement>) => {
         e.preventDefault();
@@ -123,13 +165,31 @@ export function CreateWallpaperDialog(props: CreateWallpaperDialogProps) {
         setIsOver(false);
     }, []);
 
-    const handleSave = useCallback((e: React.FormEvent) => {
-        e.preventDefault();
-        toast.success("保存成功")
-        console.log("save")
-    }, []);
+    //form.file变化后自动上传
+    useEffect(() => {
+        if (uploadingFile) {
+            uploadFile(uploadingFile);
+        }
+    }, [uploadFile, uploadingFile]);
 
-    return <Dialog open={props.open} onOpenChange={(e) => props.onChange(e)} >
+    function onSubmit(data: z.infer<typeof formSchema>) {
+        if (!data.title)
+            data.title = "未命名";
+        console.log(data);
+        if (uploading) {
+            toast.info("导入中，请稍等");
+            return;
+        }
+        toast.success(`保存成功，${JSON.stringify(data)}`);
+    }
+
+    return <Dialog open={props.open} onOpenChange={(e) => {
+        if (!e && uploading) {
+            confirm("导入未完成，确定要关闭吗？") && props.onChange(e);
+            return;
+        }
+        props.onChange(e);
+    }} >
         <DialogTrigger asChild>
             <Button
                 aria-label="创建壁纸"
@@ -153,48 +213,87 @@ export function CreateWallpaperDialog(props: CreateWallpaperDialogProps) {
             </Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
-            <form onSubmit={handleSave}>
-                <DialogHeader>
-                    <DialogTitle>创建壁纸</DialogTitle>
-                    <DialogDescription>
-                        本地壁纸，仅保存在你本机
-                    </DialogDescription>
-                </DialogHeader>
-                <div className="mt-2 mb-3">
-                    <div className="flex flex-col space-y-4">
-                        <Input id="title" placeholder="输入标题" autoComplete="off" />
-                        {!uploadedFile
-                            ?
-                            <>
-                                <div
-                                    onDragOver={handleDragOver}
-                                    onDragLeave={handleDragLeave}
-                                    onDrop={handleDrop}
-                                    className={cn(["flex flex-col items-center justify-center w-full h-32 border-2 border-dashed rounded-md cursor-pointer hover:border-primary hover:bg-muted", {
-                                        "border-primary bg-muted": isOver,
-                                    }])}>
-                                    <UploadCloudIcon className="text-foreground w-10 h-10" />
-                                    <p className="text-gray-500">{isOver ? "释放鼠标" : " 点击上传或拖入文件到这里"}</p>
-                                </div>
-                                {uploading && <Progress value={progress} />}
-                            </>
-                            :
-                            <>
-                                <h4 className="text-[#9CA3AF] mb-2">已上传文件:</h4>
-                                <div className="flex justify-between items-center text-[#9CA3AF]">
-                                    <p>{uploadedFile}</p>
-                                    <Button variant="ghost" onClick={() => setUploadedFile(undefined)}>
-                                        <DeleteIcon className="h-6 w-6" />
-                                    </Button>
-                                </div>
-                            </>
-                        }
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <DialogHeader>
+                        <DialogTitle>创建壁纸</DialogTitle>
+                        <DialogDescription>
+                            本地壁纸，仅保存在你本机
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="mt-2 mb-3">
+                        <div className="flex flex-col space-y-4">
+                            <FormField
+                                control={form.control}
+                                name="title"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <Input autoFocus placeholder="输入标题" {...field} autoComplete="off" />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="file"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <Input {...field}
+                                                type="file"
+                                                value={undefined}
+                                                ref={fileInputRef}
+                                                style={{ display: 'none' }}
+                                                onChange={(e) => {
+                                                    const file = e.target.files ? e.target.files[0] : null;
+                                                    field.onChange(file);
+                                                }
+                                                } />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            {!uploadedFile
+                                ?
+                                <>
+                                    {!uploading ?
+                                        <div
+                                            onClick={() => { fileInputRef.current?.click() }}
+                                            onDragOver={handleDragOver}
+                                            onDragLeave={handleDragLeave}
+                                            onDrop={handleDrop}
+                                            className={cn(["flex flex-col items-center justify-center w-full h-32 border-2 border-dashed rounded-md cursor-pointer hover:border-primary hover:bg-muted", {
+                                                "border-primary bg-muted": isOver,
+                                            }])}>
+                                            <UploadCloudIcon className="text-foreground w-10 h-10" />
+                                            <p className="text-gray-500">{isOver ? "释放鼠标" : " 点击导入或拖入文件到这里"}</p>
+                                        </div>
+                                        :
+                                        <div>
+                                            <span> 导入中...</span>
+                                            <Progress className="mt-2" value={progress} />
+                                        </div>}
+                                </>
+                                :
+                                <>
+                                    <h4 className="text-[#9CA3AF] mb-2">已导入文件:</h4>
+                                    <div className="flex justify-between items-center text-[#9CA3AF]">
+                                        <p>{uploadedFile}</p>
+                                        <Button type="button" variant="ghost" onClick={() => setUploadedFile(undefined)}>
+                                            <DeleteIcon className="h-6 w-6" />
+                                        </Button>
+                                    </div>
+                                </>
+                            }
+                        </div>
                     </div>
-                </div>
-                <DialogFooter >
-                    <Button type="submit">保存</Button>
-                </DialogFooter>
-            </form>
+                    <DialogFooter >
+                        <Button type="submit">保存</Button>
+                    </DialogFooter>
+                </form>
+            </Form>
         </DialogContent>
     </Dialog >
 }
