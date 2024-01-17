3f40456aafa841484e4715158aadaaa90f708297
DaZiYuan <mscoder@hotmail.com>
2024-01-17 23:39:10 +0800

    chore:分离playback-progress.tsx

--- src/giantapp-wallpaper-ui/src/app/(local)/_components/tool-bar/index.tsx
+++ src/giantapp-wallpaper-ui/src/app/(local)/_components/tool-bar/index.tsx
@@ -23,7 +23,7 @@ import {
     Reply,
 } from "lucide-react"
 import { useDebouncedCallback } from 'use-debounce';
-import { VideoSlider } from "./video-slider";
+import PlaybackProgress from "./playback-progress";
 
 interface ToolBarProps extends React.HTMLAttributes<HTMLElement> {
     playingPlaylist: Playlist[]
@@ -36,72 +36,6 @@ type PlaylistWrapper = {
     current: Wallpaper | null;
     playlist?: Playlist;
     screen: Screen;
-}
-
-//把秒转换成00:00格式
-function formatTime(seconds: number) {
-    var min = Math.floor(seconds / 60);
-    var sec = Math.floor(seconds % 60);
-    return `${min < 10 ? '0' + min : min}:${sec < 10 ? '0' + sec : sec}`;
-}
-
-const PlaybackProgress = ({ screenIndex }: { screenIndex?: number }) => {
-    console.log("PlaybackProgress:", screenIndex)
-    const [time, setTime] = useState('00:00');
-    const [totalTime, setTotalTime] = useState('00:00');
-    const [progress, setProgress] = useState(0);
-    //记录设置进度的时间
-    const [lastSetTime, setLastSetTime] = useState<number>(0);
-
-    const callApi = useDebouncedCallback((value) => {
-        console.log("setProgress:", value);
-        api.setProgress(value[0], screenIndex);
-    }, 300
-    );
-
-    const handleDrag = useCallback((value: number[]) => {
-        setLastSetTime(Date.now());
-        setProgress(value[0]);
-        callApi(value);
-    }, [callApi]);
-
-    useEffect(() => {
-        const fetch = async () => {
-            if ((document as any).shell_hidden)
-                return;
-            try {
-                //如果小于设置时间x秒，直接返回
-                var tmp = Date.now() - lastSetTime;
-                if (tmp < 2000) {
-                    return;
-                }
-
-                const res = await api.getWallpaperTime(screenIndex);
-                // console.log("getWallpaperTime:", res);
-
-                if (res.data) {
-                    setTime(formatTime(res.data.position));
-                    setTotalTime(formatTime(res.data.duration));
-                    setProgress(res.data.position / res.data.duration * 100);
-                }
-                else {
-                    setTime('00:00');
-                    setTotalTime('00:00');
-                }
-            } catch (error) {
-                console.error('Failed to fetch time:', error);
-            }
-        }
-        fetch();//先立即执行一次
-        const timer = setInterval(fetch, 1000);
-        return () => clearInterval(timer); // cleanup on component unmount
-    }, [lastSetTime, screenIndex]);
-
-    return <div className="flex items-center justify-between text-xs w-full select-none">
-        <div className="text-primary-600 dark:text-primary-400">{time}</div>
-        <VideoSlider max={100} min={0} step={1} value={[progress]} className="w-full h-1 mx-4 rounded" onValueChange={handleDrag} />
-        <div className="text-primary-600 dark:text-primary-400">{totalTime}</div>
-    </div>
 }
 
 export function ToolBar({ playingPlaylist, screens, onChangeVolume, onChangePlaylist }: ToolBarProps) {
@@ -476,6 +410,27 @@ export function ToolBar({ playingPlaylist, screens, onChangeVolume, onChangePlay
                                     and remove your data from our servers.
                                 </SheetDescription>
                             </SheetHeader>
+                            {/* 选中播放列表的壁纸列表 */}
+                            <div className="flex flex-col space-y-2">
+                                {selectedPlaylist?.playlist?.wallpapers.map((item, index) => {
+                                    return <div key={index} className="flex items-center p-0 m-0" >
+                                        <picture onClick={() => setSelectedPlaylist(null)} className="mr-2">
+                                            <img
+                                                alt="Cover"
+                                                title={item.meta?.title}
+                                                className="rounded-lg object-scale-cover aspect-square"
+                                                height={50}
+                                                src={item.coverUrl || "/wp-placeholder.webp"}
+                                                width={50}
+                                            />
+                                        </picture>
+                                        <div className="flex flex-col text-sm truncate">
+                                            <div className="font-semibold">{item?.meta?.title}</div>
+                                            <div >{item?.meta?.description}</div>
+                                        </div>
+                                    </div>
+                                })}
+                            </div>
                         </SheetContent>
                     </Sheet>
                 </>
