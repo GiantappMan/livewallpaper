c6dc0f96bf5b190503c2818b6ef1956fcc3e99c2
DaZiYuan <mscoder@hotmail.com>
2024-02-27 01:02:42 +0800

    release:v3.0.12-alpha.12商店版

--- src/giantapp-wallpaper-ui/src/app/[lang]/(local)/_components/video-slider.tsx
+++ src/giantapp-wallpaper-ui/src/app/[lang]/(local)/_components/video-slider.tsx
@@ -20,11 +20,11 @@ const VideoSlider = React.forwardRef<
       onMouseLeave={() => setIsHovered(false)}
       {...props}
     >
-      <SliderPrimitive.Track className="relative h-full w-full grow overflow-hidden rounded-full bg-primary/20">
+      <SliderPrimitive.Track className="relative  cursor-pointer h-full w-full grow overflow-hidden rounded-full bg-primary/20">
         <SliderPrimitive.Range className="absolute h-full bg-primary" />
       </SliderPrimitive.Track>
       {isHovered && (
-        <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+        <SliderPrimitive.Thumb className="block cursor-pointer h-4 w-4 rounded-full border border-primary/50 bg-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
       )}
     </SliderPrimitive.Root>
   )
