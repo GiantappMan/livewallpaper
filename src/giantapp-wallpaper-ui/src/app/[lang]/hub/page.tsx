247230141b7c8448f222e918183349984bdc7bb4
DaZiYuan <mscoder@hotmail.com>
2024-03-12 23:53:56 +0800

    chore:新版社区地址

--- src/giantapp-wallpaper-ui/src/app/[lang]/hub/page.tsx
+++ src/giantapp-wallpaper-ui/src/app/[lang]/hub/page.tsx
@@ -6,18 +6,8 @@ import { getGlobal } from "@/i18n-config";
 
 const Page = () => {
     const dictionary = getGlobal();
-    return <div onMouseEnter={() => console.log('Mouse entered')} className="p-6">
-        <div className="pl-4">
-            {dictionary['hub'].still_in_development}<br />
-            {dictionary['hub'].download_wallpaper_with_v2}<br />
-            {dictionary['hub'].set_same_directory_for_wallpapers_2_and_3}<br />
-        </div>
-        <Button variant="outline">
-            <Link target="_blank" href="https://afdian.net/item/608aae78d46b11eda1035254001e7c00">
-                {dictionary['hub'].membership_upgrade_to_v3}
-            </Link>
-        </Button>
-    </div>;
+    return <iframe className="w-full min-h-[100vh]" src="https://wallpaper.giantapp.cn/zh/hub"></iframe>;
+
 };
 
 export default Page;
