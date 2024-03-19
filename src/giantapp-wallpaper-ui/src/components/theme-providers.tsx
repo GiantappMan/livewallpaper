86149ceed433685d4ed6e9a97c608e9f38f6d30c
DaZiYuan <mscoder@hotmail.com>
2024-03-19 23:54:26 +0800

    fix:继续尝试修复主题问题

--- src/giantapp-wallpaper-ui/src/components/theme-providers.tsx
+++ src/giantapp-wallpaper-ui/src/components/theme-providers.tsx
@@ -1,17 +1,19 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
 import { ThemeProviderProps } from "next-themes/dist/types"
 import { TooltipProvider } from "@radix-ui/react-tooltip"
 import { defaultConfig, useConfig } from '@/hooks/use-config';
-import { useCallback, useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
 import api from "@/lib/client/api";
 import { ConfigAppearance } from "@/lib/client/types/config"
 import { Toaster } from "sonner"
 import { setGlobal } from "@/i18n-config";
 
 export function ThemeProvider({ children, dictionary, ...props }: { children: React.ReactNode, dictionary: any } & ThemeProviderProps) {
+    const { setTheme: setMode, resolvedTheme: mode } = useTheme()
+    const [defaultTheme, setDefaultTheme] = useState<"system" | "light" | "dark">();
     setGlobal(dictionary);
     const [_, setConfig] = useConfig()
     //读取主题设置
@@ -21,10 +23,12 @@ export function ThemeProvider({ children, dictionary, ...props }: { children: Re
 
         //尝试修复偶尔主题色不一致
         if (data) {
-            localStorage.setItem("theme", JSON.stringify(data.mode));
+            localStorage.setItem("theme", data.mode);
+            setDefaultTheme(data.mode);
+            setMode(data.mode);
         }
         return data;
-    }, [setConfig]);
+    }, [setConfig, setMode]);
 
     useEffect(() => {
         fetchConfig();
@@ -37,9 +41,10 @@ export function ThemeProvider({ children, dictionary, ...props }: { children: Re
 
     return (
         <>
-            <NextThemesProvider {...props}>
+            {defaultTheme && <NextThemesProvider {...props}>
                 <TooltipProvider>{children}</TooltipProvider>
             </NextThemesProvider>
+            }
             <Toaster closeButton={true} position="top-center" />
         </>
     )
