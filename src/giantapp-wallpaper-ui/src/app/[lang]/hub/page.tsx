363276dba825b1cfca887fea73e6972444f1897e
DaZiYuan <mscoder@hotmail.com>
2024-03-24 23:03:27 +0800

    chore:社区隐藏header

--- src/giantapp-wallpaper-ui/src/app/[lang]/hub/page.tsx
+++ src/giantapp-wallpaper-ui/src/app/[lang]/hub/page.tsx
@@ -4,13 +4,12 @@ import { useEffect, useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton"
 import { Locale } from "@/i18n-config";
 
+
 const Page = ({ params }: {
     params: { lang: Locale };
 }) => {
     const [loading, setLoading] = useState(true);
     const [iframeSrc, setIframeSrc] = useState<string | undefined | null>();
-    //打开详情dialog
-    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         const tmpParams = new URLSearchParams(window.location.search);
@@ -20,34 +19,37 @@ const Page = ({ params }: {
         }
         else {
             const defaultMode = localStorage.getItem("theme") || "system";
-            const url = `${process.env.NEXT_PUBLIC_HUB_Address}/${params.lang}/hub?mode=${defaultMode}`;
+            const url = `${process.env.NEXT_PUBLIC_HUB_Address}/${params.lang}/hub?mode=${defaultMode}&no_header=true`;
             setIframeSrc(url);
         }
     }, [params.lang]);
 
     return (
-        <div className="w-full min-h-[100vh]">
-            {loading &&
-                <div className="grid grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 p-4 overflow-y-auto max-h-[100vh] pb-20 h-ful">
-                    {
-                        Array.from({ length: 12 }).map((_, i) => (
-                            <div className="flex flex-col space-y-3" key={i}>
-                                <Skeleton className="h-[180px]  rounded-xl" />
-                                <div className="space-y-2">
-                                    <Skeleton className="h-4 w-4/5" />
-                                    <Skeleton className="h-4 w-3/5" />
-                                </div>
-                            </div>
-                        ))}
-                </div>}
+        <>
 
-            {iframeSrc && <iframe
-                allowFullScreen={true}
-                className={`w-full min-h-[100vh] ${loading ? 'hidden' : 'block'}`}
-                src={iframeSrc}
-                onLoad={() => setLoading(false)}
-            />}
-        </div>
+            <div className="w-full min-h-[100vh]">
+                {loading &&
+                    <div className="grid grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 p-4 overflow-y-auto max-h-[100vh] pb-20 h-ful">
+                        {
+                            Array.from({ length: 12 }).map((_, i) => (
+                                <div className="flex flex-col space-y-3" key={i}>
+                                    <Skeleton className="h-[180px]  rounded-xl" />
+                                    <div className="space-y-2">
+                                        <Skeleton className="h-4 w-4/5" />
+                                        <Skeleton className="h-4 w-3/5" />
+                                    </div>
+                                </div>
+                            ))}
+                    </div>}
+
+                {iframeSrc && <iframe
+                    allowFullScreen={true}
+                    className={`w-full min-h-[100vh] ${loading ? 'hidden' : 'block'}`}
+                    src={iframeSrc}
+                    onLoad={() => setLoading(false)}
+                />}
+            </div>
+        </>
     );
 };
 
