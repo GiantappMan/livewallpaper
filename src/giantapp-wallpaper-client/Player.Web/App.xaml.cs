729caddeb062edf89b04eb20d0202c89fe06ff12
DaZiYuan <mscoder@hotmail.com>
2024-04-02 00:10:32 +0800

    chore:创建外部player工程

---
+++ src/giantapp-wallpaper-client/Player.Web/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Player.Web
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
