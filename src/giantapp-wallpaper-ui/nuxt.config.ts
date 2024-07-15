b878bca32c1cbef96c9e238b4b4e7fd70d88d0e9
DaZiYuan <mscoder@hotmail.com>
2024-07-15 22:58:09 +0800

    chore:引用依赖库

--- src/giantapp-wallpaper-ui/nuxt.config.ts
+++ src/giantapp-wallpaper-ui/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  compatibilityDate: "2024-07-03",
+  devtools: { enabled: true },
+  extends: ['@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/ui',
+    '@vueuse/nuxt',
+  ],
+  future: {
+    compatibilityVersion: 4
+  },
 })
