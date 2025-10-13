a062adc0f390cffc3d63449a3f60ca864973e739
DaZiYuan <mscoder@hotmail.com>
2025-10-13 21:28:00 +0800

    wip:外部MPV

---
+++ LiveWallpaperEngineAPI/Libs/IpcClient.cs
@@ -0,0 +1,79 @@
+﻿using NLog;
+using System;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Giantapp.LiveWallpaper.Engine.Libs
+{
+    public class IpcPayload
+    {
+        [JsonPropertyName("command")]
+        public object[]? Command { get; set; }
+        [JsonPropertyName("request_id")]
+        public string? RequestId { get; set; }
+        [JsonPropertyName("data")]
+        public object? Data { get; set; }
+    }
+
+    public class IpcClient : IDisposable
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private readonly string _serverName;
+
+        public IpcClient(string serverName)
+        {
+            _serverName = serverName;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public IpcPayload? Send(IpcPayload ipcPayload)
+        {
+            string sendContent = "";
+            try
+            {
+                sendContent = JsonSerializer.Serialize(ipcPayload) + "\n";
+                using NamedPipeClientStream pipeClient = new(_serverName);
+                pipeClient.Connect(0); // 连接超时时间
+
+                if (pipeClient.IsConnected)
+                {
+                    byte[] commandBytes = Encoding.UTF8.GetBytes(sendContent);
+                    pipeClient.Write(commandBytes, 0, commandBytes.Length);
+
+                    // 读取响应
+                    byte[] buffer = new byte[4096];
+                    int bytesRead = pipeClient.Read(buffer, 0, buffer.Length);
+
+                    // 将字节数组转换为字符串
+                    string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    //减少打印
+                    //if (!sendContent.Contains("duration") && !sendContent.Contains("time-pos"))
+                    //{
+                    //_logger.Info(sendContent + "mpv response: " + response);
+                    Debug.WriteLine(sendContent + "mpv response: " + response);
+                    //}
+                    if (string.IsNullOrEmpty(response))
+                        return null;
+                    var res = JsonSerializer.Deserialize<IpcPayload>(response);
+                    return res;
+                }
+                else
+                {
+                    _logger.Warn("Failed to connect to mpv.");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Warn(ex, sendContent + "Failed to get mpv info.");
+                return null;
+            }
+        }
+    }
+}
---
+++ LiveWallpaperEngineAPI/Models/Models.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using WinAPI.Desktop.API;
+
+namespace Giantapp.LiveWallpaper.Engine.Models
+{
+
+    public enum PlayMode
+    {
+        //顺序播放
+        Order,
+        //随机播放
+        Random,
+        //定时切换
+        Timer
+    }
+    //一个壁纸的设置
+    public class WallpaperSetting : ICloneable
+    {
+        /// <summary>
+        /// 播放时长，没设就用默认值，图片默认一小时
+        /// 除了playlist，都显示这个值
+        /// </summary>
+        public string? Duration { get; set; }
+
+        #region exe
+        /// <summary>
+        /// 是否支持鼠标事件，exe和web才行。其他类型设置无效
+        /// </summary>
+        public bool EnableMouseEvent { get; set; } = false;
+        #endregion
+
+        #region video
+
+        /// <summary>
+        /// 是否启用硬件解码，video才行。其他类型无效
+        /// </summary>
+        public bool HardwareDecoding { get; set; } = true;
+
+        /// <summary>
+        /// 是否铺满
+        /// </summary>
+        public bool IsPanScan { get; set; } = true;
+
+        ///// <summary>
+        ///// 音量0-100
+        ///// </summary>
+        //public int Volume { get; set; } = 0;
+
+        ////界面不做展示，仅当VideoPlayer为默认时，读取数据并保存
+        //public VideoPlayer DefaultVideoPlayer { get; set; } = VideoPlayer.MPV_Player;
+
+        #endregion
+
+        #region web
+
+        #endregion
+
+        #region img
+
+        public DesktopWallpaperPosition Fit { get; set; } = DesktopWallpaperPosition.DWPOS_FILL;
+
+        //退出程序后，保留壁纸
+        public bool KeepWallpaper { get; set; } = true;
+
+        #endregion
+
+        #region playlist
+        public PlayMode PlayMode { get; set; } = PlayMode.Order;
+        #endregion
+
+        public static WallpaperSetting From(Dictionary<string, object> dic)
+        {
+            //根据反射，自动把字典转换为对象
+            var setting = new WallpaperSetting();
+            var type = setting.GetType();
+            foreach (var item in dic)
+            {
+                var property = type.GetProperty(item.Key);
+                if (property != null)
+                {
+                    var value = item.Value;
+                    property.SetValue(setting, value);
+                }
+            }
+            return setting;
+        }
+
+        public object Clone()
+        {
+            return CloneObject(this)!;
+        }
+
+        public static object? CloneObject(object obj)
+        {
+            if (obj == null)
+                return null;
+
+            //用json序列化clone，反射有时候要崩
+            return JsonSerializer.Deserialize(JsonSerializer.Serialize(obj), obj.GetType());
+        }
+    }
+}
