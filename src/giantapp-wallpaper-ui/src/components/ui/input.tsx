2a7255a1351631cd9407027f75e989e83a5edc3e
Meteor2333 <Meteor23333@outlook.com>
2025-12-07 16:08:24 +0800

    fix:GiantappMan#131

--- src/giantapp-wallpaper-ui/src/components/ui/input.tsx
+++ src/giantapp-wallpaper-ui/src/components/ui/input.tsx
@@ -10,6 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <input
         type={type}
+        step="1"
         className={cn(
           "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className
